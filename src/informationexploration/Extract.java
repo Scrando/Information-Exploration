1aa40e61bb24f33ea981b872d7816c5c96cbf30b
Brandon <Brandon@192.168.0.13>
2018-03-11 13:19:58 -0600

    JSON Extract

--- src/informationexploration/Extract.java
+++ src/informationexploration/Extract.java
@@ -1,9 +1,37 @@
 package informationexploration;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 /**
  *
  * @author Brandon
  */
 public class Extract {
+    
+    public void Extract(){
+        try{
+            URL url = new URL("http://api.nobelprize.org/v1/laureate.json");
+            URLConnection con = url.openConnection();
+            InputStream is =con.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+            String line = null;
+    
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e){
+            System.err.println("Caught IOException: " + e.getMessage());
+        }
+        
+        
+    }
     
 }
